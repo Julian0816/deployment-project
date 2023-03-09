208fbd3f6631cf23d3076f33148ea5f2211ff97a
Julian0816 <83423059+Julian0816@users.noreply.github.com>
2023-03-09 13:55:54 +0000

    error hanldle middleware set up

--- stock_api/handlers/reportHandler.js
+++ stock_api/handlers/reportHandler.js
@@ -1,30 +1,37 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc    Get Reports
 // @route   Get /api/reports
 // @access  Private
-const getReports = (req, res) => {
+const getReports = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Get Reports" });
-}
+});
 
 // @desc    Create Report
 // @route   Post /api/report
 // @access  Private
-const createReport = (req, res) => {
-    res.status(200).json({ message: "Create Report" });
-}
+const createReport = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+
+  res.status(200).json({ message: "Create Report" });
+});
 
 // @desc    GUpdate Reports
 // @route   Put /api/report/:id
 // @access  Private
-const updateReport = (req, res) => {
-    res.status(200).json({ message: `Update Report ${req.params.id}` });
-}
+const updateReport = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Update Report ${req.params.id}` });
+});
 
 // @desc    Delete Reports
 // @route   Delete /api/report:/id
 // @access  Private
-const deleteReport = (req, res) => {
-    res.status(200).json({ message: `Delete Report ${req.params.id}` });
-}
+const deleteReport = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete Report ${req.params.id}` });
+});
 
 module.exports = {
     getReports,
--- stock_api/server.js
+++ stock_api/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 3000
 
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/reports', require('./routes/reportRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () =>
   console.log(`server listening on http://localhost:${port}`)
