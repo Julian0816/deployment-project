7603f3239525694b4769e9e319c559892f37ac92
Julian0816 <83423059+Julian0816@users.noreply.github.com>
2023-03-14 22:58:08 +0000

    response for chicken report completed on swagger documentation, boom

--- stock_api/routes/chickenRoutes.js
+++ stock_api/routes/chickenRoutes.js
@@ -11,8 +11,10 @@ const { protect } = require('../middleware/authMiddleware')
 
 router.get("/chicken", protect, getDailyChickenReports);
 router.post("/sendchicken", protect, createDailyChickenReport);
+router.put("/chicken/:id", protect, updateDailyChickenReport);
+router.delete("/chicken/:id", protect, deleteDailyChickenReport);
 
 // router.route("/chicken").get(protect, getDailyChickenReports).post(protect, createDailyChickenReport);
-router.route("/chicken/:id").put(protect, updateDailyChickenReport).delete(protect, deleteDailyChickenReport);
+// router.route("/chicken/:id").put(protect, updateDailyChickenReport).delete(protect, deleteDailyChickenReport);
 
 module.exports = router
