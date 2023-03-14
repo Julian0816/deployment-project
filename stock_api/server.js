0f8057313ab87bc197b92f7300b47708d2f32cf6
Julian0816 <83423059+Julian0816@users.noreply.github.com>
2023-03-14 11:11:21 +0000

    Added swagger documentation 2.0 but not happy with it, I will try 3.0 in a different branch

--- stock_api/server.js
+++ stock_api/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const { errorHandler } = require('./middleware/errorMiddleware')
 const colors = require('colors')
 const connectDB = require('./config/db')
@@ -12,12 +14,15 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use("/api-docs", swaggerUi.serve);
 app.use('/api/reports', require('./routes/chickenRoutes'))
 app.use('/api/reports', require('./routes/drinksRoutes'))
 app.use('/api/reports', require('./routes/keyItemsRoutes'))
 app.use('/api/restaurants', require('./routes/restaurantRoutes'))
 
 app.use(errorHandler)
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () =>
   console.log(`server listening on http://localhost:${port}`)
